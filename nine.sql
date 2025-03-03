7122d192d041cbcc5af7288eaaa1a4dea678222c
suhas-uiux <166362686+suhas-uiux@users.noreply.github.com>
2025-03-03 23:44:08 +0530

    Create nine.sql

---
+++ nine.sql
@@ -0,0 +1,31 @@
+-- Weather table:
+-- +----+------------+-------------+
+-- | id | recordDate | temperature |
+-- +----+------------+-------------+
+-- | 1  | 2015-01-01 | 10          |
+-- | 2  | 2015-01-02 | 25          |
+-- | 3  | 2015-01-03 | 20          |
+-- | 4  | 2015-01-04 | 30          |
+-- +----+------------+-------------+
+-- Output: 
+-- +----+
+-- | id |
+-- +----+
+-- | 2  |
+-- | 4  |
++----+
+
+
+
+
+
+
+
+
+
+
+SELECT s1.id
+FROM Weather s1
+JOIN Weather s2 
+ON s1.recordDate = s2.recordDate + INTERVAL '1' DAY
+WHERE s1.temperature > s2.temperature;
